ee42e0884e0b7ddf1a43176945c185ac7f7d2895
Pierre <p.logre@hotmail.fr>
2020-12-04 17:27:27 +0100

    Export Pour PC Fixe

--- Project/script/ex_13.js
+++ Project/script/ex_13.js
@@ -1,4 +1,3 @@
-localStorage.setItem('ex_12.img', 'https://www.coding-academy.fr/wp-content/uploads/2017/10/CODING_LOGO_CMJN.png');
 let img = document.createElement("img")
 img.src = localStorage.getItem('ex_12.img')
 img.width = '500'
@@ -6,5 +5,28 @@ img.height = '250'
 document.getElementById('button').appendChild(img)
 
 $("#button").click(function() {
-    localStorage.removeItem('ex_12.img');
+    localStorage.removeItem('ex_12.img')
+    $("img").remove()
 })
+
+let count = 0
+let width = 500;
+let height = 250
+let divbutton = document.getElementById('button')
+
+divbutton.addEventListener('mouseenter', event => {
+    let interval = window.setInterval(function () {
+        count = (count-1) % 5
+        width = width + count
+        height = height + count  
+        img.style.width = width + 'px'
+        img.style.height = height + 'px'
+    }, 1000)
+    divbutton.addEventListener('mouseleave', event => {
+        window.clearInterval(interval)
+        img.style.width = '500px'
+        img.style.height = '250px'
+    })    
+})
+
+
